0ea3e8760ce544a0b8a9e6b833829fc60e2b3abb
HassanYusuf1 <112115781+HassanYusuf1@users.noreply.github.com>
2025-06-17 19:56:33 +0200

    r

--- api/Controllers/ReportsController.cs
+++ api/Controllers/ReportsController.cs
@@ -2,8 +2,8 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using NextPlay.Data;
-using NextPlay.Models;
+using api.Data;
+using api.Models;
 
 namespace api.Controllers
 {
