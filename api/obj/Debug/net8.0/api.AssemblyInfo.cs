dd06af5598d259bf2c839fea06b64dc03d8681bb
Miski Yusuf <s374221@oslomet.no>
2025-06-25 19:53:06 +0200

    eb
    eb

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e04c2124ca3ffc4a3c9934e077076333630c5e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd444396c788e75d9ad71cbde3619edcc3ae19c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
